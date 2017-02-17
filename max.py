7b6a8713acafce82a0d50ce27ec7a8c8a679e382
ss <palestinianpy@gmail.com>
2017-02-17 03:21:16 +0200

    sd

--- max.py
+++ max.py
@@ -1,129 +1,155 @@
-'''
-    Proxy Checker v2
-        -=m0dem=-
+#!/usr/bin/env python
 
-    Requirements:
-        requests (HTTP library)
-'''
-
+from argparse import ArgumentParser
 from Queue import Queue
 from threading import Thread
 
-import argparse
 import requests
 import sys
 import time
 
 
-def process_proxy():
-    try:
-        while True:
-            proxy = queue.get()
-            if proxy == "STOP":
-                return
-            
-            save_valid_proxy(check_proxy(proxy))
-            queue.task_done()
+class ProxyChecker:
+    def __init__(self, inlist, threads = 200, verbose = False, timeout = 25):
+        self.inlist = inlist
+        # there shouldn't be more threads than there are proxies
+        if threads > len(self.inlist):
+            self.threads = len(self.inlist)
 
-    except:
-        pass
+        else:
+            self.threads = threads
 
-def check_proxy(proxy, timeout = 5):
-    proxies = {
-            "http": "http://" + proxy,
-            "https": "https://" + proxy
-    }
-    try:
-        # see if the proxy actually works
-        ip = get_external_ip(proxies = proxies)          
-        if ip == ORIG_IP:
-            if VERBOSE:
-                print(ip)
-                
+        self.verbose = verbose
+        self.timeout = timeout
+
+        self.outlist = []
+        self.total_scanned = 0
+        self.total_working = 0
+        self.original_ip = None
+        self.threads_done = 0
+
+        # constants
+        self.IP_CHECK = "https://api.ipify.org"
+
+    def save_valid_proxy(self, proxy):
+        if proxy:
+            self.outlist.append(proxy)
+
+    def get_external_ip(self, proxies = None):
+        headers = {"User-Agent": "Mozilla/5.0"}
+
+        try:
+            if proxies:
+                r = requests.get(self.IP_CHECK, proxies = proxies, headers = headers, timeout = self.timeout)
+
+            else:
+                r = requests.get(self.IP_CHECK, headers = headers)
+
+        except IOError:
             return False
 
-    except IOError:
-        return False
-                
-    return proxy
+        return str(r.text)
 
-def save_valid_proxy(proxy):
-    if proxy:
-        OUT_F.write(proxy + "\n")
+    def check_proxy(self, proxy):
+        proxies = {
+                "http": "http://" + proxy,
+                "https": "https://" + proxy
+        }
 
-def get_external_ip(proxies = None):
-    headers = {"User-Agent": "Mozilla/5.0"}
-    if proxies:
-        r = requests.get(IP_CHECK, proxies = proxies, headers = headers)
+        # see if the proxy actually works
+        ip = self.get_external_ip(proxies = proxies)
+        if not ip:
+            return False
 
-    else:
-        r = requests.get(IP_CHECK, headers = headers)
-        
-    return str(r.text)
+        if ip != self.original_ip:
+            if self.verbose:
+                print(ip)
+
+            self.total_working += 1
+
+        return proxy
+
+    def process_proxy(self):
+        try:
+            while True:
+                proxy = self.queue.get()
+                self.save_valid_proxy(self.check_proxy(proxy))
+                self.queue.task_done()
+                self.total_scanned += 1
+
+        except:
+            pass
+
+    def start(self):
+        if self.verbose:
+            print("Running: {} threads...".format(self.threads))
+
+        self.original_ip = self.get_external_ip()
+
+        if self.verbose:
+            print("Your original external IP address is: {}".format(self.original_ip))
+            print("Checking proxies...")
+
+        self.queue = Queue(self.threads)
+
+        # get all our threads ready for work
+        for i in range(0, self.threads):
+            thread = Thread(target = self.process_proxy)
+            thread.daemon = True
+            thread.start()
+
+        self.start = time.time()
+
+        # keep sending threads their jobs
+        try:
+            for proxy in self.inlist:
+                self.queue.put(proxy.strip())
+
+            self.queue.join()
+
+        except KeyboardInterrupt:
+            if self.verbose:
+                print("Closing down, please let the threads finish.")
+
+        if self.verbose:
+            print("Running: {:.2f} seconds".format(time.time() - self.start))
+
+        if self.verbose:
+            print("Scanned: {} proxies".format(self.total_scanned))
+            print("Working: {} proxies".format(self.total_working))
+
+        return self.outlist
 
 
-DEFAULT_THREADS = 200
-IP_CHECK = "https://api.ipify.org"
-IN_F = None
-OUT_F = None
-VERBOSE = False
-ORIG_IP = None
+def CLI():
+    threads_default = 200
+    timeout_default = 25
 
-
-if __name__ == "__main__":
     # handle all the command-line argument stuff
-    parser = argparse.ArgumentParser(description = "Check a proxy list for working proxies.")
+    parser = ArgumentParser(description = "Check a proxy list for working proxies.")
     parser.add_argument("infile", type = str, help = "input proxy list file")
     parser.add_argument("outfile", type = str, help = "output proxy list file")
-    parser.add_argument("-t", "--threads", type = int, default = DEFAULT_THREADS, help = "set the number of threads running concurrently (default {})".format(DEFAULT_THREADS))
+    parser.add_argument("-t", "--threads", type = int, default = threads_default, help = "set the number of threads running concurrently (default {})".format(threads_default))
     parser.add_argument("-v", "--verbose", action = "store_true", help = "say lots of useless stuff (sometimes)")
+    parser.add_argument("-to", "--timeout", type = int, help = "the timeout for testing proxies (default {})".format(timeout_default))
     args = parser.parse_args()
 
+    # check to see if the input file actually exists
     try:
-        IN_F = open(args.infile, "r")
+        infile = open(args.infile, "r")
+        inlist = infile.read().split("\n")
+        infile.close()
 
     except IOError:
         print("Invalid proxy list filename.")
         sys.exit()
 
-    OUT_F = open(args.outfile, "w")
-        
-    # number of threads running at once
-    CONCURRENT_THREADS = args.threads
+    proxy_checker = ProxyChecker(inlist, threads = args.threads, verbose = args.verbose, timeout = timeout_default)
+    outlist = proxy_checker.start()
+    outfile = open(args.outfile, "w")
+    outfile.write("\n".join(outlist))
+    outfile.close()
 
-    if args.verbose:
-        VERBOSE = True
-        print("Loading proxy list from: {}".format(args.infile))
-        print("Saving valid proxies list to: {}".format(args.outfile))
-        print("Running: {} threads...".format(CONCURRENT_THREADS))
-        
-    # let's begin the whole process
-    ORIG_IP = get_external_ip()
-    if VERBOSE:
-        print("Your original external IP address is: {}".format(ORIG_IP))
-        print("Checking proxies...")
-    
-    queue = Queue(CONCURRENT_THREADS * 2)
 
-    for i in range(0, CONCURRENT_THREADS):
-        thread = Thread(target = process_proxy)
-        thread.daemon = True
-        thread.start()
-
-    start = time.time()
-    try:
-        for proxy in IN_F:
-            queue.put(proxy.strip())
-
-        # queue.join()
-
-    except KeyboardInterrupt:
-        pass
-
-    # make sure everything is closed down
-    print("Closing down, please wait. ({} seconds run time)".format(time.time() - start))
-    queue.put("STOP")
-
-    IN_F.close()
-    OUT_F.close()
-    sys.exit()
+if __name__ == "__main__":
+    CLI()
